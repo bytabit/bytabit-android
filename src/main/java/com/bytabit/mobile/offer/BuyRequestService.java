117cc1b619006802f490b3346fd95f9b905efa67
Steve Myers <steve@bytabit.org>
2017-04-13 22:52:40 -0700

    create BuyRequest from offer detail view, fix logback config

--- src/main/java/com/bytabit/mobile/offer/BuyRequestService.java
+++ src/main/java/com/bytabit/mobile/offer/BuyRequestService.java
@@ -1,26 +1,13 @@
 package com.bytabit.mobile.offer;
 
 import com.bytabit.mobile.offer.model.BuyRequest;
-import com.bytabit.mobile.offer.model.SellOffer;
 import retrofit2.Call;
-import retrofit2.http.*;
-
-import java.util.List;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface BuyRequestService {
 
-    @POST("/v1/offers")
-    Call<SellOffer> createOffer(@Body SellOffer offer);
-
     @POST("/v1/offers/{sellerEscrowPubkey}/buyRequests")
     Call<BuyRequest> createBuyRequest(@Path("sellerEscrowPubkey") String sellerEscrowPubkey, @Body BuyRequest buyRequest);
-
-    @GET("/v1/offers")
-    Call<List<SellOffer>> read();
-
-    @PUT("/v1/offers/{sellerEscrowPubkey}")
-    Call<SellOffer> update(@Path("sellerEscrowPubkey") String sellerEscrowPubkey, @Body SellOffer offer);
-
-    @DELETE("/v1/offers/{sellerEscrowPubkey}")
-    Call<SellOffer> delete(@Path("sellerEscrowPubkey") String sellerEscrowPubkey);
 }
--- src/main/java/com/bytabit/mobile/offer/SellOfferService.java
+++ src/main/java/com/bytabit/mobile/offer/SellOfferService.java
@@ -1,6 +1,5 @@
 package com.bytabit.mobile.offer;
 
-import com.bytabit.mobile.offer.model.BuyRequest;
 import com.bytabit.mobile.offer.model.SellOffer;
 import retrofit2.Call;
 import retrofit2.http.*;
@@ -11,9 +10,6 @@ public interface SellOfferService {
 
     @POST("/v1/offers")
     Call<SellOffer> createOffer(@Body SellOffer offer);
-
-    @POST("/v1/offers/{sellerEscrowPubkey}/buyRequests")
-    Call<BuyRequest> createBuyRequest(@Path("sellerEscrowPubkey") String sellerEscrowPubkey, @Body BuyRequest buyRequest);
 
     @GET("/v1/offers")
     Call<List<SellOffer>> read();
